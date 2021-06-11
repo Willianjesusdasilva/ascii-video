e072f24a43da3c4eb7bd9ac0faf7ba49ec85fb58
Willian Jesus da Silva <46569232+Willianjesusdasilva@users.noreply.github.com>
2021-06-11 11:18:05 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,27 +2,29 @@ import cv2
 import numpy as np
 import pyautogui
 import os
+from io import StringIO
 
 pyautogui.FAILSAFE = False
-pyautogui.PAUSE = 0.05
+pyautogui.PAUSE = 0
+
 
 def img_to_ascii(image):
     ASCII_CHARS = ["@", "#", "S", "%", "?", "*", "+", ";", ":", ",", "."]
     height, width = image.shape
-    height = height//2
-    width = width//2
+    height = height//3
+    width = width//3
     ratio = height/width
     new_height = int(width * ratio)
     image_resized = cv2.resize(image, (width, new_height)) 
-    characters = []
+    buff = StringIO()
     for line in image_resized:
-        characters.append("".join([ASCII_CHARS[pixel//25] for pixel in line])+"\n")
-
-    return ''.join(characters)
+        buff.write("".join([ASCII_CHARS[pixel//25] for pixel in line])+"\n")
+    
+    return buff
 
 while True:
-    image = pyautogui.screenshot(region=(552,537, 200,170))
+    image = pyautogui.screenshot(region=(550,529, 200,170))
     result = img_to_ascii(cv2.cvtColor(np.array(image), cv2.COLOR_BGR2GRAY))
     os.system('cls')
-    print(result)
+    print(result.getvalue())
 
